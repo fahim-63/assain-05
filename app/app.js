3cdbf2c8e2afdd22f457cc58de528ce06638e10f
Fahim Farhan Tanvir <165804682+fahim-63@users.noreply.github.com>
2026-03-04 01:35:24 +0600

    js added

--- app/app.js
+++ app/app.js
@@ -3,7 +3,14 @@ for(let i = 0; i < fahim.length; i++){
  const btnFahim = fahim[i];
  btnFahim.addEventListener('click',function(event)
  {
-  event.target.classList.add('bg-gray-300')
-  event.target.classList.add('border-none')
+
+    for(let a = 0; a < fahim.length; a++){
+       fahim[a].classList.remove('btn-primary')
+       fahim[a].classList.add('btn-primary')
+       
+    }
+  event.target.classList.add('bg-gray-300',"border-none",'text-black',alert('Board submission is successful'));
  })
+
+  
 }
